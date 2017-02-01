58d78a7be3e8350c8ecd5c622ccc03243f448a8d
tandris <tandris@tandris.chilisoft.org>
2017-02-01 15:08:46 +0100

    Add message event hooks

--- plugins/core/naga.js
+++ plugins/core/naga.js
@@ -1,6 +1,7 @@
 module.exports.title = 'naga-sender';
 
 const http = require('http');
+const request = require('request');
 
 let senderToken;
 let nagaMainUser;
@@ -77,54 +78,49 @@ function parseMessageId(messageId) {
 
 function sendEvent(messageId, eventType, eventMessage) {
     console.info('Sending message event. { eventType = ' + eventType + ',  messageId = ' + messageId + ', message = ' + eventMessage + ' }');
-    let req = http.request({
-        host: nagaMainHost,
-        protocol: nagaMainProtocol,
-        port: nagaMainPort,
-        path: '/api/sender/unit/add_message_event',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'X-Auth-Token': senderToken
+    request(nagaMainProtocol + '//' + nagaMainHost + ':' + nagaMainPort + '/api/sender/unit/add_message_event', {
+            json: true,
+            method: 'POST',
+            headers: {
+                'X-Auth-Token': senderToken
+            },
+            body: {
+                messageId: messageId,
+                eventType: eventType,
+                eventMessage: eventMessage
+            }
         },
-        agent: false // create a new agent just for this one request
-    }, (res) => {
-        res.on("data", function(chunk) {
-            let result = JSON.parse(chunk);
-            console.log(result);
-        });
-    });
-    req.on('error', function(err) {
-        console.error('Failed to send event request.');
-        console.log(err);
-    });
-    req.end();
+        (err, res, body) => {
+            if (err) {
+                console.error('Failed to send message event. { messageId = ' + messageId + '}');
+                console.error(err);
+            } else if (body.success === false) {
+                console.error('Faliled to send message event, invalid message event. { messageId = ' + messageId + '}');
+            } else {
+                console.info('Message event sent. { messageId = ' + messageId + '}');
+            }
+        }
+    );
+
 }
 
 function heartbeat() {
-    let req = http.request({
-        host: nagaMainHost,
-        protocol: nagaMainProtocol,
-        port: nagaMainPort,
-        path: '/api/sender/unit/heartbeat',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'X-Auth-Token': senderToken
-        },
-        agent: false // create a new agent just for this one request
-    }, (res) => {
-        res.on("data", function(chunk) {
-            let result = JSON.parse(chunk);
-            if (result !== true) {
-                console.error('Failed to send heartbeat to naga main server.');
-                console.error(result);
+    request(nagaMainProtocol + '//' + nagaMainHost + ':' + nagaMainPort + '/api/sender/unit/heartbeat', {
+            json: false,
+            method: 'POST',
+            headers: {
+                'X-Auth-Token': senderToken
             }
-        });
-    });
-    req.on('error', function(err) {
-        console.error('Failed to send heartbeat request to server.');
-        console.log(err);
-    });
-    req.end();
+        },
+        (err, res, body) => {
+            if (err) {
+                console.error('Failed to send heartbeat signal.');
+                console.error(err);
+            } else if (body.success === false) {
+                console.error('Failed to send heartbeat signal, invalid content.');
+            } else {
+                console.info('Heartbeat signal sent.');
+            }
+        }
+    );
 }
